c92e2dcd36af210433de209a8b672faedbf3d955
Ricardo Rojas Rico <rojasricor@gmail.com>
2023-04-10 17:58:38 -0500

    Create workspace.sh

---
+++ workspace.sh
@@ -0,0 +1,8 @@
+git clone https://github.com/rojasricor/nodejs-rsystfip && git clone https://github.com/rojasricor/api-client-rsystfip;
+
+sudo apt update && sudo apt upgrade -y;
+
+sudo apt install apache2 mariadb-server php -y;
+
+curl -sL https://deb.nodesource.com/setup_19.x | sudo -E bash -;
+sudo apt install nodejs -y;
