e7ea3dedae9fd5ee3b9387866961f0bb2ace1581
Ricardo Rojas Rico <rojasricor@gmail.com>
2023-04-11 15:21:55 -0500

    Update workspace.sh

--- workspace.sh
+++ workspace.sh
@@ -4,5 +4,16 @@ sudo apt update && sudo apt upgrade -y;
 
 sudo apt install apache2 mariadb-server php -y;
 
+sudo apt install curl -y;
+
 curl -sL https://deb.nodesource.com/setup_19.x | sudo -E bash -;
 sudo apt install nodejs -y;
+
+sudo apt install wget -y;
+
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/sublimehq-archive.gpg > /dev/null;
+
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list;
+sudo apt update && sudo apt install sublime-text;
+
+
